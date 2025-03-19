1c354f3930be3883ecc127ecb25b2319fc18cb75
IsrafilMatter <israfildayopalabay21@gmail.com>
2025-03-19 14:49:43 +0800

    pseudocode

---
+++ reverse_character_case.py
@@ -0,0 +1,10 @@
+# Program to reverse the case of each character
+# Author: Israfil Palabay
+# Date: March 19, 2025
+
+# Ask the user to enter their full name with incorrect casing
+# Read the input string from the user
+# Swap the case of each character
+# - Uppercase letters become lowercase
+# - Lowercase letters become uppercase
+# Print the modified name
