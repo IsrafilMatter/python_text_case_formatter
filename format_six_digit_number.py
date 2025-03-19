072ec220c944582ceb50c10f6957f49ecda1ab0f
IsrafilMatter <israfildayopalabay21@gmail.com>
2025-03-19 16:57:43 +0800

    Asking the user to enter a number between 0 and 1000

--- format_six_digit_number.py
+++ format_six_digit_number.py
@@ -3,7 +3,9 @@
 # Date: March 19, 2025
 
 # Ask the user to enter a number between 0 and 1000
+number = int(input("Enter a number (0-1000): "))
+
 # Convert the input to an integer
-# Format the number to be displayed with exactly 6 digits, 
+# Format the number to be displayed with exactly 6 digits
 # With leading zeros if necessary
 # Print the formatted number
