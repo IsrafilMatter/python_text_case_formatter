fc7e4863f4351933bff69e6a5341145c5d6a5ddc
IsrafilMatter <israfildayopalabay21@gmail.com>
2025-03-19 14:57:46 +0800

    pseudocode

---
+++ convert_to_snake_case.py
@@ -0,0 +1,9 @@
+# Program to convert text to snake case
+# Author: Israfil Palabay
+# Date: March 19, 2025
+
+# Ask the user to enter their name with incorrect casing
+# Read the input string from the user
+# Convert all characters to lowercase
+# Split the name into words and joining the words with underscores
+# Print the snake_case formatted name.
