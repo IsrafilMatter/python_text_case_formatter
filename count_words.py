de07699141ec1be4689e9c7fbbffa52da14adb0b
IsrafilMatter <israfildayopalabay21@gmail.com>
2025-03-19 16:33:34 +0800

    Spliting the statement into words and counting the number of words and print the results

--- count_words.py
+++ count_words.py
@@ -2,8 +2,8 @@
 # Author: Israfil Palabay
 # Date: March 19, 2025
 
-# Ask the user to enter a enter a complete statement
+# Ask the user to enter a complete statement
 statement = input("Enter a complete statement: ")
 
 # Split the statement into words
-# Count the number of words and print the result
+print(len(statement.split())) # Count the number of words and print the result
