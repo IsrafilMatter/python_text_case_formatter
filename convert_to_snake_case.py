c48a1d625b52138e95ff413285608145caf22be6
IsrafilMatter <israfildayopalabay21@gmail.com>
2025-03-19 15:40:22 +0800

    Asking the user to enter their name with incorrect casing

--- convert_to_snake_case.py
+++ convert_to_snake_case.py
@@ -3,6 +3,8 @@
 # Date: March 19, 2025
 
 # Ask the user to enter their name with incorrect casing
+name = input("Enter your fullname in incorrect casing: ")
+
 # Read the input string from the user
 # Convert all characters to lowercase
 # Split the name into words and joining the words with underscores
