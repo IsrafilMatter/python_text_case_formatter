1a2e5a9d0b6c7c45854473177292d5c9498899d5
IsrafilMatter <israfildayopalabay21@gmail.com>
2025-03-19 14:34:06 +0800

    pseudocode

---
+++ format_six_digit_number.py
@@ -0,0 +1,9 @@
+# Program to format number to 6 digits
+# Author: Israfil Palabay
+# Date: March 19, 2025
+
+# Ask the user to enter a number between 0 and 1000
+# Convert the input to an integer
+# Format the number to be displayed with exactly 6 digits, 
+# With leading zeros if necessary
+# Print the formatted number
