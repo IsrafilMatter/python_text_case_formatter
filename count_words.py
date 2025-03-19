778f233917ef8331ebfe8f2eedd3ae2c2b2ec23a
IsrafilMatter <israfildayopalabay21@gmail.com>
2025-03-19 14:46:57 +0800

    pseudocode

---
+++ count_words.py
@@ -0,0 +1,8 @@
+# Program to count words in a statement
+# Author: Israfil Palabay
+# Date: March 19, 2025
+
+# Ask the user to enter a enter a complete statement
+# Read the input string from the user
+# Split the statement into words
+# Count the number of words and print the result
