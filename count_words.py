701548eb1eb1ef08471478949ba3cef17360824a
IsrafilMatter <israfildayopalabay21@gmail.com>
2025-03-19 16:20:21 +0800

    Ask the user to enter a complete statement

--- count_words.py
+++ count_words.py
@@ -3,6 +3,7 @@
 # Date: March 19, 2025
 
 # Ask the user to enter a enter a complete statement
-# Read the input string from the user
+statement = input("Enter a complete statement: ")
+
 # Split the statement into words
 # Count the number of words and print the result
