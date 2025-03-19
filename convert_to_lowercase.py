090ff13e240d614a4aaf6143ad59217134c90c6e
IsrafilMatter <israfildayopalabay21@gmail.com>
2025-03-19 15:04:28 +0800

    Asking the user to input their name in Incorrect casing

--- convert_to_lowercase.py
+++ convert_to_lowercase.py
@@ -3,6 +3,8 @@
 # Date: March 19, 2025
 
 # Ask the user to enter their name with incorrect casing
+name = input("Enter your fullname: ")
+
 # Read the input string from the user
 # Convert all characters to lowercase
 # Print the lowercase version of the name
