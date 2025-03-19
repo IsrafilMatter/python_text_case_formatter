24f160bd069f9fa66d40a2fcc0ed108a83b185d2
IsrafilMatter <israfildayopalabay21@gmail.com>
2025-03-19 16:09:31 +0800

    Asking the user to enter their name

--- convert_to_uppercase.py
+++ convert_to_uppercase.py
@@ -3,6 +3,8 @@
 # Date: March 19, 2025
 
 # Ask the user to enter their name
+name = input("Enter your fullname: ")
+
 # Read the input string from the user
 # Convert all characters in the name to uppercase
 # Print the uppercase version of the name
