f3ac3a6de8c7314be12b921772a899861043c6cf
IsrafilMatter <israfildayopalabay21@gmail.com>
2025-03-19 15:01:40 +0800

    pseudocode

---
+++ convert_to_pascal_case.py
@@ -0,0 +1,9 @@
+# Program to convert text to pascal case
+# Author: Israfil Palabay
+# Date: March 19, 2025
+
+# Ask the user to enter their name with incorrect casing
+# Read the input string from the user
+# Convert the text to title case
+# Remove spaces by splitting the words and joining them back
+# Print the PascalCase formatted name
