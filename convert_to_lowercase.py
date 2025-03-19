7d1a4b6f2f04dc0eaf6cab749025e56d9337ebc6
IsrafilMatter <israfildayopalabay21@gmail.com>
2025-03-19 15:02:59 +0800

    pseudocode

---
+++ convert_to_lowercase.py
@@ -0,0 +1,8 @@
+# Program to convert text to lowercase
+# Author: Israfil Palabay
+# Date: March 19, 2025
+
+# Ask the user to enter their name with incorrect casing
+# Read the input string from the user
+# Convert all characters to lowercase
+# Print the lowercase version of the name
