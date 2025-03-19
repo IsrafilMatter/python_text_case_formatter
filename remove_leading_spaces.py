a62db639c064120d5e7d76b8896db3a5e57e5c58
IsrafilMatter <israfildayopalabay21@gmail.com>
2025-03-19 14:30:04 +0800

    pseudocode

---
+++ remove_leading_spaces.py
@@ -0,0 +1,8 @@
+# Program to remove leading spaces from input
+# Author: Israfil Palabay
+# Date: March 19, 2025
+
+# Ask the user to enter their name with leading spaces
+# Read the input string from user
+# Remove any leading spaces from the input the 'lstrip()' function
+# Print the name with no spaces
