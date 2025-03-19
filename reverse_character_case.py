e4add0ced7daccfb7f2b4a0d174c0de9b75115fa
IsrafilMatter <israfildayopalabay21@gmail.com>
2025-03-19 17:20:00 +0800

    Asking the User to input their fullname with incorrect casing

--- reverse_character_case.py
+++ reverse_character_case.py
@@ -3,7 +3,8 @@
 # Date: March 19, 2025
 
 # Ask the user to enter their full name with incorrect casing
-# Read the input string from the user
+name = input("Enter your fullname in incorrect casing: ")
+
 # Swap the case of each character
 # - Uppercase letters become lowercase
 # - Lowercase letters become uppercase
