7f0354de31fbf81570b5d0153bdfc7d91f3bfb6b
IsrafilMatter <israfildayopalabay21@gmail.com>
2025-03-19 14:53:44 +0800

    pseudocode

---
+++ count_characters.py
@@ -0,0 +1,8 @@
+# Program to count characters in a name
+# Author: Israfil Palabay
+# Date: March 19, 2025
+
+# Ask the user to enter their full name
+# Read the input string from user
+# Count the total number of characters including spaces
+# Print the character count
